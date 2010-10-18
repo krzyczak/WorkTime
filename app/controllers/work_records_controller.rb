36a47a45267b770e3f10ee4954fb0aa786e847ea
krzyczak <not_set>
2010-10-18 14:09:10 +0200

    Added choose employee in new_work_record form

--- app/controllers/work_records_controller.rb
+++ app/controllers/work_records_controller.rb
@@ -5,7 +5,6 @@ class WorkRecordsController < ApplicationController
     if params[:report] == nil
       params[:report] = Hash.new
       params[:report][:start_date] = "#{Date.today.day}-#{Date.today.month}-#{Date.today.year}"
-      #params[:report_monthly] = "false"
       params[:department_id] = 1
       
       params[:report][:monthly] = "false"
@@ -14,15 +13,14 @@ class WorkRecordsController < ApplicationController
     start_date = Date.parse(params[:report][:start_date])
     end_date = start_date
     
-    #if params[:report_monthly] == "true"
     if params[:report][:monthly] == "true"
       last_day = Date.civil(start_date.year, start_date.month, -1).day
       start_date = Date.civil(start_date.year, start_date.month, 1)
       end_date = Date.civil(start_date.year, start_date.month, last_day)
     end
-    department_id = params[:department_id]
+    department_id = params[:department_id].to_i
     employees = Employee.where(:department_id => department_id)
-    work_records = WorkRecord.where(:department_id => department_id)
+    #work_records = WorkRecord.where(:department_id => department_id)
     #@work_records = WorkRecord.where(:department_id => department_id).where("date >= ?", start_date).where("date <= ?", end_date)
     @work_records = WorkRecord.where(:department_id => department_id).where("date >= ?", start_date).where("date <= ?", end_date)
     .select("SUM(gr3) as gr3_sum")
@@ -68,8 +66,14 @@ class WorkRecordsController < ApplicationController
   end
 
   def new
+    @employees = get_employees_for_report
+    
+    if params[:employee_id].nil?
+      params[:employee_id] = @employees.first.id
+    end
+    
     @work_record = WorkRecord.new
-    @work_record.employee = Employee.find(session[:employee_id])
+    @work_record.employee = Employee.find(params[:employee_id])
     @work_record.date = session[:report_date]
     @submit_button_text = "Dalej"
   end
@@ -81,16 +85,15 @@ class WorkRecordsController < ApplicationController
 
   def create
     @work_record = WorkRecord.new(params[:work_record])
-    @work_record.department = Department.find(session[:department])
-    #@work_record.breaks = @work_record.calculate_breaks
+    @work_record.department = Department.find(session[:department_id])
 
     if @work_record.save
       if @work_record.employee_id == Employee.last.id
-        session[:employee_id] = nil
-        redirect_to(work_records_path, :notice => 'Pomyślnie dodano wpis.')
+        #session[:employee_id] = nil
+        redirect_to(work_records_path, :notice => t(:successfully_added_work_record))
       else
-        session[:employee_id] = next_employee_id
-        redirect_to(new_work_record_path, :notice => 'Pomyślnie dodano wpis.')
+        #session[:employee_id] = next_employee_id
+        redirect_to(new_work_record_path, :notice => t(:successfully_added_work_record))
       end
     else
       @submit_button_text = "Dalej"
@@ -102,8 +105,6 @@ class WorkRecordsController < ApplicationController
     @work_record = WorkRecord.find(params[:id])
 
     if @work_record.update_attributes(params[:work_record])
-      #@work_record.breaks = @work_record.calculate_breaks
-      #@work_record.save!
       redirect_to(@work_record, :notice => 'Pomyślnie zaktualizowano wpis.')
     else
       @submit_button_text = "Zatwierdź"
@@ -124,26 +125,32 @@ class WorkRecordsController < ApplicationController
   
   def create_report_configuration
     #this method should be optimized and cleaned in the future
-    session[:department] = params[:department].to_i
-    @employees = Employee.where(:department_id => session[:department])
+    session[:department_id] = params[:department_id].to_i
     
     report_date = Date.parse(params[:report_configuration][:date])
     session[:report_date] = report_date
-    last_employee_id = (WorkRecord.where(:date => report_date, :department_id => session[:department]).count == 0 ? 0 : WorkRecord.where(:date => report_date).last.employee_id)
     
-    @employees = (last_employee_id == 0) ? @employees : get_employees_for_report(@employees, last_employee_id)
+    work_records_done = WorkRecord.where(:department_id => session[:department_id]).where(:date => session[:report_date])
     
-    if @employees.count == 0
-      redirect_to(work_records_path, :notice => "Na dzień #{report_date}, dla działu #{Department.find(session[:department]).name} wprowadzono już dane wszystkich pracowników.")
+    if work_records_done.count == Employee.where(:department_id => session[:department_id]).count
+      msg = "Na dzień #{report_date}, dla działu #{Department.find(session[:department_id]).name}
+      wprowadzono już dane wszystkich pracowników."
+      redirect_to(work_records_path, :notice => msg)
     else
-      session[:employee_id] = @employees.first.id
       redirect_to new_work_record_path
     end
   end
   
   private
   
-  def get_employees_for_report(employees, last_employee_id)
+  def get_employees_for_report
+    work_records_done = WorkRecord.where(:date => session[:report_date]).where(:department_id => session[:department_id])
+    employees_done = []
+    work_records_done.each {|wr| employees_done.push wr.employee }
+    return Employee.where(:department_id => session[:department_id]).all.select {|e| employees_done.include?(e) == false }
+  end
+  
+  def get_employees_for_report1(employees, last_employee_id)
     #this method should be optimized in the future
     employees.each do |e|
       employees = employees.reverse
@@ -159,7 +166,7 @@ class WorkRecordsController < ApplicationController
   
   def next_employee_id
     #this method should be optimized in the future
-    employees = Employee.where(:department_id => session[:department])
+    employees = Employee.where(:department_id => session[:department_id])
     get_employees_for_report(employees, session[:employee_id]).first.id
   end
 end
