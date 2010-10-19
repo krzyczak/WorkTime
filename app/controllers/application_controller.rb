50ddb1369966a8fdc35582711a764882f5b9d11f
krzyczak <not_set>
2010-10-19 16:06:54 +0200

    Added NavigationHelper and tweaked work_records edit to get back from where it started. Also added AddNewEmployee button to employee show action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  include NavigationHelper
+  
   def last_day(date)
     last_day = Date.civil(date.year, date.month, -1).day
   end
---
+++ app/helpers/navigation_helper.rb
@@ -0,0 +1,14 @@
+module NavigationHelper
+  #def self.included(controller)
+    #controller.send :helper, :redirect_to_target_or_default, :store_target_location
+  #end
+  
+  def redirect_to_target_or_default(default, notice)
+    redirect_to(session[:return_to] || default, notice)
+    session[:return_to] = nil
+  end
+  
+  def store_target_location
+    session[:return_to] = request.request_uri
+  end
+end
