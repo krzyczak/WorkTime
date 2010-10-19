a0155e1677788f68d3e22f5361a0329d5f0428a5
krzyczak <not_set>
2010-10-19 15:35:37 +0200

    Cleaned up migrations a little bit

--- db/migrate/20101012143045_add_department_to_employee.rb
+++
@@ -1,9 +0,0 @@
-class AddDepartmentToEmployee < ActiveRecord::Migration
-  def self.up
-    add_column :employees, :department_id, :integer
-  end
-
-  def self.down
-    remove_column :employees, :department_id
-  end
-end
