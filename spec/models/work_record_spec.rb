85e1efec77c9e0ebe0233fa6e31058210d254099
KrzyczaK <krzyczak@krzyczak-pc.(none)>
2010-10-29 17:18:10 +0200

    Fixed all tests, but we still need to thing of some mocking features, as (at least I think so) model test should be unit test and should not depend on DB (as it is now).

--- spec/models/work_record_spec.rb
+++ spec/models/work_record_spec.rb
@@ -2,30 +2,33 @@ require 'spec_helper'
 
 describe WorkRecord do
   before(:each) do
-    @w = WorkRecord.new
-  #  Banner.delete_all
-  #  @valid_attributes = {
-  #    :name => "banner_name",
-  #    :max_height => 1,
-  #    :max_width => 1,
-  #    :structure => false
-  #  }
+    @wr = WorkRecord.new
   end
 
   it "should return correct value when counting accord from all groups" do
-    @w.accord_all_groups.should == 0.0
+    @wr.accord_all_groups.should == 0.0
     
-    @w.gr3 = @w.gr4 =  @w.gr5 =  @w.gr6 =  @w.gr7 =  @w.gr8 =  @w.gr9 = 2.5
-    @w.accord_all_groups.should == 17.5
+    @wr.gr3 = @wr.gr4 =  @wr.gr5 =  @wr.gr6 =  @wr.gr7 =  @wr.gr8 =  @wr.gr9 = 2.5
+    @wr.accord_all_groups.should == 17.5
   end
   
   it "should return correct productivity" do
-    @w.all_work_time = 0
-    @w.productivity.should == "0.0 %"
+    @wr.all_work_time = 0
+    @wr.productivity.should == "0.0 %"
     
-    @w.all_work_time = 465.0
-    @w.gr3 = @w.gr4 =  @w.gr5 =  @w.gr6 =  @w.gr7 =  @w.gr8 =  @w.gr9 = (465.0/7)
-    @w.layover = (465.0/7)
-    @w.productivity.should == "116.67 %"
+    @wr.all_work_time = 465.0
+    @wr.gr3 = @wr.gr4 =  @wr.gr5 =  @wr.gr6 =  @wr.gr7 =  @wr.gr8 =  @wr.gr9 = (465.0/7)
+    @wr.layover = (465.0/7)
+    @wr.productivity.should == "116.67 %"
+  end
+  
+  it "should return 15 minutes of breaks when all work time is greater than 360 minutes" do
+    @wr.all_work_time = 361
+    @wr.calculate_breaks.should == 15
+  end
+  
+  it "should return 0 minutes of breaks when all work time is smaller than or equal to 360 minutes" do
+    @wr.all_work_time = 360
+    @wr.calculate_breaks.should == 0
   end
 end
